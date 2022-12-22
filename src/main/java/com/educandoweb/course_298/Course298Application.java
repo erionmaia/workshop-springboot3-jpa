45ff4b59d504a79f9fe498e471a16e6aa638847b
Erion Schlenger de Paiva Maia <erionmaia@gmail.com>
2022-12-22 14:13:46 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course_298/Course298Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course_298;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course298Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course298Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/course_298/Course298ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course_298;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course298ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
